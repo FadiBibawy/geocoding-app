60943e830e151d807bc24bf682ec71136f5ea4f9
FadiBibawy <eng.fadimichel@gmail.com>
2023-08-06 16:36:29 +0200

    added a public dir

---
+++ express.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const publicDIR = path.join(__dirname, "/public");
+
+// const options = {
+//   dotfiles: "ignore",
+//   etag: false,
+//   extensions: ["htm", "html"],
+//   index: false,
+//   maxAge: "1d",
+//   redirect: false,
+//   setHeaders(res, path, stat) {
+//     res.set("x-timestamp", Date.now());
+//   },
+// };
+
+app.use(express.static(publicDIR));
+
+app.get("/", (req, res) => {
+  res.send("this is the homeee");
+});
+app.get("/help", (req, res) => {
+  res.send("this is the helpt");
+});
+app.get("/about", (req, res) => {
+  res.send("this is the about");
+});
+app.get("/weather", (req, res) => {
+  res.send("<h1>WEATHER Pageeeeeeeeeeeee</h1>");
+});
+
+// app.listen(3005);
